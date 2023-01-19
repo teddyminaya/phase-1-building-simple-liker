ff4b5f42bdc3c0a53c7d31766305e97f91202f0b
Teddy Minaya <teddyminaya@gmail.com>
2023-01-19 12:24:40 -0500

    commit tree 050bb448850297a58c6d5103d3b71aa1e28b1710

--- main.js
+++ main.js
@@ -3,9 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {hearts()});
 
+function hearts(){
+  const likeButton = document.querySelectorAll("span.like.glyph")
+  
+  likeButton.forEach(likeListener)
 
-
+function likeListener(eachButton){
+  eachButton.addEventListener("click", (e) => {
+    mimicServerCall()
+    .then((response) => {
+      if (e.target.innerText === EMPTY_HEART) {
+        e.target.innerText = FULL_HEART
+        e.target.classList.add("activated-heart")
+    }
+    else if (e.target.innerText === FULL_HEART) {
+      e.target.innerText === FULL_HEART
+      e.target.classList.remove("activated-heart")
+    }
+    })
+  .catch(function(){
+    const errorMessage = document.getElementById("modal")
+    console.log(errorMessage)
+    errorMessage.className = "" 
+  setTimeout(() => {errorMessage.className = "hidden"}, 3000)})
+  })
+}};
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
